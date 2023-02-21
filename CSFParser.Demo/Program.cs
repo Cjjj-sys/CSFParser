ba1fac633aeb7d5a326929459c826e4393a82a8a
KangKang <44329867+Cjjj-sys@users.noreply.github.com>
2023-02-21 04:51:00 +0000

    add reading labels test

--- CSFParser.Demo/Program.cs
+++ CSFParser.Demo/Program.cs
@@ -12,6 +12,26 @@ class Program {
         var identifier = header.GetRange(0, 4);
         Console.WriteLine(CheckIdentifier(identifier));
         PrintDebug(identifier);
+        var body = csfBytes.Where((e, i) => i >= 0x18).ToList();
+        List<CSFLabel> labels = new();
+        int state = 0;
+        for (int i = 4; i < body.Count; i += 4)
+        {
+            var part = body.GetRange(i - 4, i).ToArray();
+            switch (state)
+            {
+                case 0:
+                    if (Bytes2Str(part) == " LBL")
+                    {
+                        state = 1;
+                    }
+                    break;
+                case 1:
+                    
+
+            }
+            
+        }
     }
 
     static void PrintDebug(List<Byte> bytes) {
@@ -28,6 +48,15 @@ class Program {
     static string Bytes2Str(List<Byte> bytes) {
         StringBuilder sb = new();
         bytes.ForEach(e => {
+            var ch = Char.ConvertFromUtf32(e);
+            sb.Append(ch);
+        });
+        return sb.ToString();
+    }
+
+    static string Bytes2Str(Byte[] bytes) {
+        StringBuilder sb = new();
+        bytes.ToList().ForEach(e => {
             var ch = Char.ConvertFromUtf32(e);
             sb.Append(ch);
         });
---
+++ CSFParser.Lib/Models/CSFLabel.cs
@@ -0,0 +1,24 @@
+
+/**
+    Label header
+    The label data begins with a label header, which is built up like this:
+
+    Offset	Type	Description
+    0x0	char[4]	" LBL"
+    Label identifier
+    If this is not " LBL", the game will not recognize the following data as label data and read the next 4 bytes.
+    0x4	DWORD	Number of string pairs
+    This is the number of string pairs associated with this label. Usual value is 1.
+    0x8	DWORD	LabelNameLength
+    This value holds the size of the label name that follows.
+    0xC	char[LabelNameLength]	LabelName
+    A non-zero-terminated string that is as long as the DWORD at 0x8 says. If it is longer, the rest will be cut off.
+    The first label in ra2md.csf can be found at 0x18.
+    Note: Spaces, tabs and line breaks will be formatted out of the label's name, therefore they cannot be used. However, although spaces will be formatted out, there is a label named "gui:password entry box label" in the ra2.csf and ra2md.csf file that comes with the game.
+    Note: The label name is case-insensitive. If a label name is shown up for more than once, the last item will actually be loaded by the game.
+**/
+public class CSFLabel {
+    int NumberOfStringPairs {get;set;}
+    int LabelNameLength {get;set;}
+    string LabelName {get;set;}
+}
